dc7df6ff837991a5f042d4045feaa31552e67870
mengxue xu <mxu4321@gmail.com>
2023-01-21 16:40:09 -0800

    installed express.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // --- import dependencies ---
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+// --- create the connection to database ---
+const db = mysql.createConnection(
+    { 
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'employee_management_db'
+    },
+    console.log(`Connected to the employees database.`)
+);
+
+// --- prompt user for what they would like to do ---
