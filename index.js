54699f58de8730ed19c339e71c2020ff6309f388
mengxue xu <mxu4321@gmail.com>
2023-02-14 20:08:11 -0800

    added demo video

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ const mainMenu = () => {
       type: "list",
       message: "What would you like to do",
       choices: [
-        "View All Employees", // ✅
-        "View All Employees By Department", // ✅
-        "View All Employees By Manager", // ✅
-        "Add Employee", // ✅
-        "Remove Employee", // ✅
-        "Update Employee Role", // ✅
-        "Update Employee Manager", // ✅
-        "View All Roles", // ✅
-        "Add Role", // ✅
-        "Remove Role", // ✅
-        "View All Departments", // ✅
-        "Add Department", // ✅
-        "Remove Department", // ✅
-        "View Total Utilized Budget of a Department", // ✅
-        "Exit", // ✅
+        "View All Employees", 
+        "View All Employees By Department", 
+        "View All Employees By Manager", 
+        "Add Employee", 
+        "Remove Employee", 
+        "Update Employee Role", 
+        "Update Employee Manager", 
+        "View All Roles", 
+        "Add Role", 
+        "Remove Role", 
+        "View All Departments", 
+        "Add Department", 
+        "Remove Department", 
+        "View Total Utilized Budget of a Department", 
+        "Exit", 
       ],
     })
     .then((answer) => {
